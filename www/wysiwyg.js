3ccecd90c1546dd45219b784189a1a9f4ee0e951
Vitaly Egorov <egorov@samsonos.com>
2015-10-06 17:29:12 +0300

    Added new wysiwyg

--- www/wysiwyg.js
+++ www/wysiwyg.js
@@ -1,70 +1,83 @@
 $(document).ready(function() {
     $('.__wysiwyg .__input').summernote({
+        toolbar: [
+            ['style', ['style']],
+            ['text', ['bold', 'italic', 'underline', 'color', 'clear']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['height', ['height']],
+            ['size', ['fontsize']],
+            ['font', ['fontname']],
+        ],
+        fontsize: '18px',
+        onblur: function() {
+            var text = $('#editor').code();
+            text = text.replace("<br>", " ");
+            $('#description').val(text);
+        },
         height: 425                 // set editor height
     });
 });
 
-s('.__wysiwyg .__input').pageInit( function( textarea )
-{
-	/*// Create Select field instance with save handler
-	s('.__wysiwyg .__input').each(function(textarea)
-	{*/
-		var field = textarea.parent();
+s('.__wysiwyg .__input').pageInit( function( textarea ) {
+    /*// Create Select field instance with save handler
+     s('.__wysiwyg .__input').each(function(textarea)
+     {*/
+    var field = textarea.parent();
 
-		// Hide span
-		s('span',field).hide();
+    // Hide span
+    s('span', field).hide();
 
-		// Current value view
-		var sp = s('.note-editable.panel-body',field);
+    s('.note-current-fontsize').show();
+    s('.note-current-fontname').show();
+    s('[data-name=color] .caret').show();
 
-		// Add special class to display as it was on site
-		sp.addClass('cms-content');
+    // Current value view
+    var sp = s('.note-editable.panel-body', field);
 
-		// Original value
-		var ov = s('.__hidden',field);
+    // Add special class to display as it was on site
+    sp.addClass('cms-content');
 
-		// Save handler
-		sp.blur(function(tb)
-		{
-			// Create loader object
-			var loader = new Loader( field );
+    // Original value
+    var ov = s('.__hidden', field);
 
-			// Get current tb value
-			var new_value = tb.html();
+    // Save handler
+    sp.blur(function (tb) {
+        // Create loader object
+        var loader = new Loader(field);
 
-			// If value changed
-			if( new_value !== ov.val() )
-			{
-				sp.hide();
-				loader.show();
+        // Get current tb value
+        var new_value = tb.html();
 
-				// Create form for async post
-				form = s('<form method="post" enctype="multipart/form-data"></form>');
+        // If value changed
+        if (new_value !== ov.val()) {
+            sp.hide();
+            loader.show();
 
-				// Set field action as form action
-				form.a('action', s('input[name="__action"]', field).val());
+            // Create form for async post
+            form = s('<form method="post" enctype="multipart/form-data"></form>');
 
-				// Add all field hidden fields to form
-				s('input[type="hidden"]', field).each(function(hidden)
-				{
-					form.append('<textarea name="'+hidden.a('name')+'">'+hidden.val()+'</textarea>');
-				});
+            // Set field action as form action
+            form.a('action', s('input[name="__action"]', field).val());
 
-				// Add new field value
-				form.append('<textarea name="__value">'+new_value+'</textarea>');
+            // Add all field hidden fields to form
+            s('input[type="hidden"]', field).each(function (hidden) {
+                form.append('<textarea name="' + hidden.a('name') + '">' + hidden.val() + '</textarea>');
+            });
 
-				// Perform ajax save
-				form.ajaxForm(function(responce)
-				{
-					// Hide loader
-					loader.remove();
+            // Add new field value
+            form.append('<textarea name="__value">' + new_value + '</textarea>');
 
-					sp.show();
+            // Perform ajax save
+            form.ajaxForm(function (responce) {
+                // Hide loader
+                loader.remove();
 
-					// Save new value as original value
-					ov.val( new_value );
-				});
-			}
-		});
-	/*});*/
+                sp.show();
+
+                // Save new value as original value
+                ov.val(new_value);
+            });
+        }
+    });
+    /*});*/
 });
